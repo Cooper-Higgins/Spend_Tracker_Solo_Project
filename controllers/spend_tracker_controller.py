36f6050cc3e939f04d155261573e1daf38d1f27d
Cooper-Higgins <jamie@supplantree.com>
2023-04-14 17:58:03 +0100

    Basic nav functionality w/ links working to all core pages

--- controllers/spend_tracker_controller.py
+++ controllers/spend_tracker_controller.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.transaction import Transaction
+from models.user import User
 import repositories.transaction_repository as transaction_repository
 import repositories.user_repository as user_repository
 
 spend_tracker_blueprint = Blueprint("spend_tracker", __name__)
 
-@spend_tracker_blueprint.route("/home")
-def all_transactions():
-    transactions = transaction_repository.select_all()
-    return render_template("home/index.html", all_transactions = transactions)
+@spend_tracker_blueprint.route("/")
+def index():
+    return render_template("/index.html")
+
+@spend_tracker_blueprint.route("/transactions/")
+def transactions():
+    return render_template("/transactions/index.html")
+
+@spend_tracker_blueprint.route("/merchants/")
+def merchants():
+    return render_template("/merchants/index.html")
+
+@spend_tracker_blueprint.route("/categories/")
+def categories():
+    return render_template("/categories/index.html")
+
+@spend_tracker_blueprint.route("/account/")
+def account():
+    return render_template("/account/index.html")
