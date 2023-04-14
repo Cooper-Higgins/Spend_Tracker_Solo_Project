b7d89ddbca9717504ea09e4e01e3f5318064d84c
Cooper-Higgins <jamie@supplantree.com>
2023-04-14 13:54:50 +0100

    Caught typo in __init__.py

--- models/transaction.py
+++ models/transaction.py
@@ -1,5 +1,5 @@
 class Transaction:
-    def __nit__(self, tx_value, merchant, category, time_stamp, id = None):
+    def __init__(self, tx_value, merchant, category, time_stamp, id = None):
         self.tx_value = tx_value
         self.merchant = merchant
         self.category = category
