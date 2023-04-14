122ff2e267fa3f2b721af66154cf70ac4cda4727
Cooper-Higgins <jamie@supplantree.com>
2023-04-14 14:13:49 +0100

    Create (save) methods added to user and transaction repositories

--- repositories/transaction_repository.py
+++ repositories/transaction_repository.py
@@ -2,3 +2,11 @@ from db.run_sql import run_sql
 
 from models.transaction import Transaction
 from models.user import User
+
+def create(transaction):
+    sql = "INSERT INTO transactions (tx_value, merchant, category, time_stamp, user_id) VALUES (%s, %s, %s, %s, %s) RETURNING *"
+    values = [transaction.tx_value, transaction.merchant, transaction.category, transaction.time_stamp, transaction.user.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    transaction.id = id
+    return transaction
--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -2,3 +2,11 @@ from db.run_sql import run_sql
 
 from models.user import User
 from models.transaction import Transaction
+
+def save(user):
+    sql = "INSERT INTO users (first_name, last_name, dob, city, email, budget) VALUES (%s, %s, %s, %s, %s, %s) RETURNING *"
+    values = [user.first_name, user.last_name, user.dob, user.city, user.email, user.budget]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    user.id = id
+    return user
