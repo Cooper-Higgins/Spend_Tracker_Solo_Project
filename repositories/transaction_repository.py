3ad94098ef5839d1783c9e0f541e696475259e9f
Cooper-Higgins <jamie@supplantree.com>
2023-04-14 14:19:33 +0100

    Select all function added to tx & user repos

--- repositories/transaction_repository.py
+++ repositories/transaction_repository.py
@@ -2,6 +2,7 @@ from db.run_sql import run_sql
 
 from models.transaction import Transaction
 from models.user import User
+import repositories.user_repository as user_repository
 
 def create(transaction):
     sql = "INSERT INTO transactions (tx_value, merchant, category, time_stamp, user_id) VALUES (%s, %s, %s, %s, %s) RETURNING *"
@@ -10,3 +11,15 @@ def create(transaction):
     id = results[0]['id']
     transaction.id = id
     return transaction
+
+def select_all():
+    transactions = []
+
+    sql = "SELECT * FROM transactions"
+    results = run_sql(sql)
+
+    for row in results:
+        user = user_repository.select(row['user_id'])
+        transaction = Transaction(row['tx_value'], row['merchant'], row['category'], row['time_stamp'], user, row['id'] )
+        transactions.append(transaction)
+    return transactions
--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -3,10 +3,21 @@ from db.run_sql import run_sql
 from models.user import User
 from models.transaction import Transaction
 
-def save(user):
+def create(user):
     sql = "INSERT INTO users (first_name, last_name, dob, city, email, budget) VALUES (%s, %s, %s, %s, %s, %s) RETURNING *"
     values = [user.first_name, user.last_name, user.dob, user.city, user.email, user.budget]
     results = run_sql(sql, values)
     id = results[0]['id']
     user.id = id
     return user
+
+def select_all():
+    users = []
+
+    sql = "SELECT * FROM users"
+    results = run_sql(sql)
+
+    for row in results:
+        user = User(row['first_name'], row['last_name'], row['dob'], row['city'], row['email'], row['budget'], row['id'] )
+        users.append(user)
+    return users
