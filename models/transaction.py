1ec83e5a3cfb1de5bf6bc2fc3a655fc34250b5bd
Cooper-Higgins <jamie@supplantree.com>
2023-04-14 13:41:46 +0100

    Transaction class complete in models

---
+++ models/transaction.py
@@ -0,0 +1,7 @@
+class Transaction:
+    def __nit__(self, tx_value, merchant, category, time_stamp, id = None):
+        self.tx_value = tx_value
+        self.merchant = merchant
+        self.category = category
+        self.time_stamp = time_stamp
+        self.id = id
