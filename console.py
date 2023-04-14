a93a5eae29e24f027c03450a71744b0299c565ac
Cooper-Higgins <jamie@supplantree.com>
2023-04-14 15:13:31 +0100

    Users and transactions populating as expected (after grief)

--- console.py
+++ console.py
@@ -1,9 +1,8 @@
-import pdb
-from models.user import User
 from models.transaction import Transaction
+from models.user import User
 
-import repositories.user_repository as user_repository
 import repositories.transaction_repository as transaction_repository
+import repositories.user_repository as user_repository
 
 transaction_repository.delete_all()
 user_repository.delete_all()
@@ -11,7 +10,7 @@ user_repository.delete_all()
 user_1 = User('Sam', 'Fire', '01/01/1990', 'Glasgow', 'sam.fire@gmail.com', 1000.00)
 user_repository.create(user_1)
 
-transaction_1 = Transaction(10.00, 'Lidl', 'Supermarket', '01/04/2023')
-transaction_repository.create(transaction_1)
+user_repository.select_all()
 
-pdb.set_trace()
+transaction_1 = Transaction(10.00, 'Lidl', 'Supermarket', '01/04/2023', user_1)
+transaction_repository.create(transaction_1)
--- models/transaction.py
+++ models/transaction.py
@@ -1,7 +1,8 @@
 class Transaction:
-    def __init__(self, tx_value, merchant, category, time_stamp, id = None):
+    def __init__(self, tx_value, merchant, category, time_stamp, user, id = None):
         self.tx_value = tx_value
         self.merchant = merchant
         self.category = category
         self.time_stamp = time_stamp
+        self.user = user
         self.id = id
